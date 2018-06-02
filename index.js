7651cb1fdb474c1a9abd6850f5e2ef00a4c1827c
subhan <dxa04099@gmail.com>
2018-06-02 13:52:56 +0700

    fix class name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { Alert, Platform} from 'react-native';
 import JWPlayer from '../nativemodules/JWPlayer';
 
-export default class JWPlayer {
+export default class JWPlayerModule {
 
   static jwPlayerIOSPLayListener:any;
   static jwPlayerIOSPauseListener:any;
@@ -16,59 +16,59 @@ export default class JWPlayer {
     if (Platform.OS === 'ios') {
 
   			// VideoPlayer.showVideoPlayer(videoUrl);
-  			if(Utils.jwPlayerIOSPLayListener) Utils.jwPlayerIOSPLayListener.remove();
+  			if(JWPlayerModule.jwPlayerIOSPLayListener) JWPlayerModule.jwPlayerIOSPLayListener.remove();
   			const jwPlayerEventEmitter = new NativeEventEmitter(JWPlayer);
-  			Utils.jwPlayerIOSPLayListener = jwPlayerEventEmitter.addListener(
+  			JWPlayerModule.jwPlayerIOSPLayListener = jwPlayerEventEmitter.addListener(
   				'jwOnPlay',
   				(playObject) => {
-  						console.log("[Utils] JWPlayer IOS playObject ", playObject);
+  						console.log("[JWPlayerModule] JWPlayer IOS playObject ", playObject);
   					}
   			);
 
-  			if(Utils.jwPlayerIOSTimerListener) Utils.jwPlayerIOSTimerListener.remove();
-  			Utils.jwPlayerIOSTimerListener = jwPlayerEventEmitter.addListener(
+  			if(JWPlayerModule.jwPlayerIOSTimerListener) JWPlayerModule.jwPlayerIOSTimerListener.remove();
+  			JWPlayerModule.jwPlayerIOSTimerListener = jwPlayerEventEmitter.addListener(
   				'jwOnTime',
   				(time) => {
-  					console.log("[Utils] JWPlayer IOS time ",moment().format("HH:mm:ss"));
+  					console.log("[JWPlayerModule] JWPlayer IOS time ",moment().format("HH:mm:ss"));
   				}
   			);
 
-  			if(Utils.jwPlayerIOSPauseListener) Utils.jwPlayerIOSPauseListener.remove();
-  			Utils.jwPlayerIOSPauseListener = jwPlayerEventEmitter.addListener(
+  			if(JWPlayerModule.jwPlayerIOSPauseListener) JWPlayerModule.jwPlayerIOSPauseListener.remove();
+  			JWPlayerModule.jwPlayerIOSPauseListener = jwPlayerEventEmitter.addListener(
   				'jwOnPause',
   				(pauseObject) => {
-  					console.log("[Utils] JWPlayer IOS Pause ", pauseObject);
+  					console.log("[JWPlayerModule] JWPlayer IOS Pause ", pauseObject);
   				}
   			);
 
-  			if(Utils.jwPlayerIOSCompleteListener) Utils.jwPlayerIOSCompleteListener.remove();
-  			Utils.jwPlayerIOSCompleteListener = jwPlayerEventEmitter.addListener(
+  			if(JWPlayerModule.jwPlayerIOSCompleteListener) JWPlayerModule.jwPlayerIOSCompleteListener.remove();
+  			JWPlayerModule.jwPlayerIOSCompleteListener = jwPlayerEventEmitter.addListener(
   				'jwOnComplete',
   				(complete) => {
-  					console.log("[Utils] JWPlayer IOS Complete ", complete);
+  					console.log("[JWPlayerModule] JWPlayer IOS Complete ", complete);
   				}
   			);
 
-  			if(Utils.jwPlayerIOSCloseListener) Utils.jwPlayerIOSCloseListener.remove();
-  			Utils.jwPlayerIOSCloseListener = jwPlayerEventEmitter.addListener(
+  			if(JWPlayerModule.jwPlayerIOSCloseListener) JWPlayerModule.jwPlayerIOSCloseListener.remove();
+  			JWPlayerModule.jwPlayerIOSCloseListener = jwPlayerEventEmitter.addListener(
   				'jwOnClose',
   				(close) => {
-  					console.log("[Utils] JWPlayer IOS Close ", close);
+  					console.log("[JWPlayerModule] JWPlayer IOS Close ", close);
   				}
   			);
 
-  			if(Utils.jwPlayerIOSErrorListener) Utils.jwPlayerIOSErrorListener.remove();
-  			Utils.jwPlayerIOSErrorListener = jwPlayerEventEmitter.addListener(
+  			if(JWPlayerModule.jwPlayerIOSErrorListener) JWPlayerModule.jwPlayerIOSErrorListener.remove();
+  			JWPlayerModule.jwPlayerIOSErrorListener = jwPlayerEventEmitter.addListener(
   				'jwOnError',
   				(error) => {
-  					console.log("[Utils] JWPlayer IOS Error ",error);
+  					console.log("[JWPlayerModule] JWPlayer IOS Error ",error);
   				}
   			);
         JWPlayer.PlayStream(videoUrl, 0);
     }else if (Platform.OS === 'android') {
-      Alert.alert('', `[Utils].playVideo Platform not yet supported ${Platform.OS}`);
+      Alert.alert('', `[JWPlayerModule].playVideo Platform not yet supported ${Platform.OS}`);
     }else {
-      Alert.alert('', `[Utils].playVideo Platform not yet supported ${Platform.OS}`);
+      Alert.alert('', `[JWPlayerModule].playVideo Platform not yet supported ${Platform.OS}`);
     }
   }
 }
